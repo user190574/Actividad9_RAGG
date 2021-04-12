a9ab373f4e1d3d1079513a5df8c0b2bcd02dfc51
user190574 <78620322+user190574@users.noreply.github.com>
2021-04-12 00:08:01 -0700

    Add files via upload

---
+++ Actividad9_RAGG/Actividad9_RAGG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad9_RAGG
+//
+//  Created by user190574 on 4/11/21.
+//  Copyright © 2021 user190574. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
